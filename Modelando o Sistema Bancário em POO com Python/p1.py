a920ff47f1caf91ee70fb95a998cf766d3cd233f
Gabriel <gabriel.serpa2010@hotmail.com>
2025-06-10 18:24:00 -0300

    Update p1.py

--- Modelando o Sistema Bancário em POO com Python/p1.py
+++ Modelando o Sistema Bancário em POO com Python/p1.py
@@ -129,4 +129,28 @@ class historico:
         )
         
 class Transacao(ABC):
-    pass
+    @property
+    @abstractproperty
+    def valor(self):
+        pass
+    
+    @abstractclassmethod
+    def registrar(self, conta):
+        pass
+
+class Saque(Transacao):
+    def __init__(self, valor):
+        self.valor = valor
+    
+    @property
+    def valor(self):
+        return self._valor
+    
+    def registrar(self, conta):
+        sucesso_transacao = conta.sacar(self.valor)
+        
+        if sucesso_transacao:
+            conta.historico.adicionar_transacao(self)
+    
+class Deposito(Transacao):
+    
